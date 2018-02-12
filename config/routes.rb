46a7439cfa2a5f9feb502d4f95478e1da15d62d7
Pradeep Gopireddy <pradeepgopireddy456@gmail.com>
2018-02-12 19:12:43 +0530

    added_route_changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
+	#root to: "devise/sessions#new"
   devise_for :users
-	root to: "users#new"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
