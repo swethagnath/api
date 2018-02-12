d747da5556257c463cba46fdd108a6bdf79b1ce8
Pradeep Gopireddy <pradeepgopireddy456@gmail.com>
2018-02-12 19:25:47 +0530

    added cuisine model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212135102) do
+ActiveRecord::Schema.define(version: 20180212135341) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20180212135102) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "city_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
